b3dded893207f65ff9ed5b51f2bcf7f1e2482810
Thanh Y <lethanhy9999@gmail.com>
2024-11-01 13:41:50 +0700

    first commit

---
+++ blockchain-code/block.cs
@@ -0,0 +1,88 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace blockchain
+{
+    public class Block<T>
+    {
+        private string? _hash;
+        private int _blockNumber;
+        private int randomValue;
+        private DateTime time;
+        private Block<T?>? block;
+        private static DateTime dateTime = DateTime.Now;
+        private static Random random = new Random();
+        private T? _data;
+        private static int staticID = 0;
+        public Block(Block<T?> _block, T? value, DateTime timeCreate)
+        {
+            this.randomValue = random.Next();
+            if (dateTime > timeCreate)
+            {
+                return;
+            }
+            else
+            {
+                dateTime = timeCreate;
+                time = dateTime;
+            }
+            _data = value;
+            block = _block;
+            this._blockNumber = staticID;
+            this._hash = this.reHash;
+        }
+        public Block(T? value, DateTime timeCreate)
+        {
+            this.time = timeCreate;
+            this._data = value;
+            this._hash = ComputeHash(value + this._blockNumber.ToString()
+                + this.randomValue.ToString() + this.time.ToString());
+            block = null;
+        }
+        private static string ComputeHash(string input)
+        {
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                byte[] bytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(input));
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    builder.Append(bytes[i].ToString("x2")); // Chuyển đổi thành chuỗi hex
+                }
+                return builder.ToString();
+            }
+        }
+        private string? getHash
+        {
+            get { return _hash; }
+        }
+        public string reHash
+        {
+            get
+            {
+                return ComputeHash(this._data?.ToString() ?? "null" + this._blockNumber.ToString() + this.block?.GetHashCode() ?? "null" + this.randomValue.ToString() + this.time.ToString());
+            }
+        }
+        public T? data
+        {
+            get
+            {
+                if (this._data == null) return default(T?); return this._data;
+            }
+        }
+        public bool checkTime(DateTime timeNew)
+        {
+            if (this.time > timeNew) return false; return true;
+        }
+        public int blockNumber
+        { get { return this._blockNumber; } }
+        public bool checkTheVaild
+        {
+            get
+            {
+                if (this.block == null) return true;
+                return (this.reHash == this._hash || this.checkTheVaild);
+            }
+        }
+    }
+}
---
+++ blockchain-code/blockchain.cs
@@ -0,0 +1,40 @@
+﻿
+namespace blockchain
+{
+    internal class Chain<T>
+    {
+        private Block<T?> block;
+        private static int quantity = 0;
+
+        public Chain(T? value, DateTime timeCreate)
+        {
+            block = new Block<T?>(value, timeCreate); quantity++;
+        }
+
+        public Chain(T? value)
+        {
+            block = new Block<T?>(value, DateTime.Now); quantity++;
+        }
+
+        public bool Add(T? value, DateTime timeCreate)
+        {
+            if (!block.checkTime(timeCreate)) return false;
+            quantity++;
+            block = new Block<T?>(this.block, value, timeCreate); return true;
+        }
+
+        public bool checkTheChainValidity
+        {
+            get
+            {
+                if (quantity <= 0) return false;
+                if (quantity != this.block.blockNumber) return false;
+                return this.block.checkTheVaild;
+            }
+        }
+
+        public int Quantity
+        { get { return quantity; } }
+
+    }
+}
